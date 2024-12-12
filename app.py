79e877b7268859fcdd7e18262e45b26bca238afa
HECTOR STEVEEN ORDOÑEZ CHAMBA <hsc.ordonez@yavirac.edu.ec>
2024-12-12 08:55:03 -0500

    fix: solucinar logica de enrutado

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-# app.py
-from flask import Flask
+from flask import Flask, redirect, url_for
 from dotenv import load_dotenv
 from modules.memes.routes import memes_routes
 
@@ -8,6 +7,11 @@ load_dotenv()
 app = Flask(__name__)
 
 app.register_blueprint(memes_routes, url_prefix='/app')
+
+# Redirigir la raíz a /app/
+@app.route('/')
+def index():
+    return redirect(url_for('memes_routes.home'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
--- modules/memes/routes.py
+++ modules/memes/routes.py
@@ -1,29 +1,13 @@
 from flask import Blueprint, render_template
 from services.meme_service import get_all_memes
 
-# Crear un blueprint para las rutas de memes
 memes_routes = Blueprint('memes_routes', __name__)
 
-# Ruta de inicio
 @memes_routes.route('/')
 def home():
     memes = get_all_memes()
     return render_template('index.html', memes=memes)
 
-# Ruta para la lista de memes
-@memes_routes.route('/listas')
-def listas():
-    memes = get_all_memes()
-    print("Datos enviados al template:", memes)
-    return render_template('pages/modules/memes/list.html', memes=memes)
-
-# Ruta para la página de memes
-@memes_routes.route('/memes')
-def memes():
-    memes = get_all_memes()
-    return render_template('pages/modules/memes/index.html', memes=memes)
-
-# Ruta para crear memes
 @memes_routes.route('/crear')
 def crear():
     return render_template('pages/modules/memes/crear.html')
